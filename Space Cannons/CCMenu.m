eed2ce32abb9b05f592842fc9940a3abce70c51d
Shayne Meyer <shayne.meyer@gmail.com>
2014-06-08 15:52:33 -0700

    Completed. menu animations. Fade out on when user clicks play. Fade in when the game is over.

--- Space Cannons/CCMenu.m
+++ Space Cannons/CCMenu.m
@@ -12,34 +12,37 @@
 {
     SKLabelNode *_scoreLabel;
     SKLabelNode *_topScoreLabel;
+    SKSpriteNode *_title;
+    SKSpriteNode *_scoreBoard;
+    SKSpriteNode *_playButton;
 }
 
 - (id)init
 {
     self = [super init];
     if (self) {
-        SKSpriteNode *title = [SKSpriteNode spriteNodeWithImageNamed:@"Title"];
-        title.position = CGPointMake(0, 140);
-        [self addChild:title];
+        _title = [SKSpriteNode spriteNodeWithImageNamed:@"Title"];
+        _title.position = CGPointMake(0, 140);
+        [self addChild:_title];
         
-        SKSpriteNode *scoreBoard = [SKSpriteNode spriteNodeWithImageNamed:@"ScoreBoard"];
-        scoreBoard.position = CGPointMake(0, 70);
-        [self addChild:scoreBoard];
+        _scoreBoard = [SKSpriteNode spriteNodeWithImageNamed:@"ScoreBoard"];
+        _scoreBoard.position = CGPointMake(0, 70);
+        [self addChild:_scoreBoard];
         
-        SKSpriteNode *playButton = [SKSpriteNode spriteNodeWithImageNamed:@"PlayButton"];
-        playButton.name = @"Play";
-        playButton.position = CGPointMake(0, 0);
-        [self addChild:playButton];
+        _playButton = [SKSpriteNode spriteNodeWithImageNamed:@"PlayButton"];
+        _playButton.name = @"Play";
+        _playButton.position = CGPointMake(0, 0);
+        [self addChild:_playButton];
         
         _scoreLabel = [SKLabelNode labelNodeWithFontNamed:@"DIN Alternate"];
         _scoreLabel.fontSize = 30;
-        _scoreLabel.position = CGPointMake(-52, 50);
-        [self addChild:_scoreLabel];
+        _scoreLabel.position = CGPointMake(-52, -20);
+        [_scoreBoard addChild:_scoreLabel];
 
         _topScoreLabel = [SKLabelNode labelNodeWithFontNamed:@"DIN Alternate"];
         _topScoreLabel.fontSize = 30;
-        _topScoreLabel.position = CGPointMake(48, 50);
-        [self addChild:_topScoreLabel];
+        _topScoreLabel.position = CGPointMake(48, -20);
+        [_scoreBoard addChild:_topScoreLabel];
         
         self.score = 0;
         self.topScore = 0;
@@ -51,13 +54,46 @@
 
 -(void)hide
 {
-    self.hidden = YES;
     self.touchable = NO;
+    
+    SKAction *animateMenu = [SKAction scaleTo:0.0 duration:0.5];
+    animateMenu.timingMode = SKActionTimingEaseIn;
+    [self runAction:animateMenu completion:^{
+        self.hidden = YES;
+        self.xScale = 1.0;
+        self.yScale = 1.0;
+    }];
+    
 }
 
 -(void)show
 {
     self.hidden = NO;
+    
+    SKAction *fadeIn = [SKAction fadeInWithDuration:0.5];
+    
+    _title.position = CGPointMake(0, 280);
+    _title.alpha = 0;
+    SKAction *animateTitle = [SKAction group:@[[SKAction moveToY:140 duration:0.5],
+                                               fadeIn]];
+    animateTitle.timingMode = SKActionTimingEaseOut;
+    [_title runAction:animateTitle];
+
+    // animate scoreboard
+    _scoreBoard.xScale = 4.0;
+    _scoreBoard.yScale = 4.0;
+    _scoreBoard.alpha = 0;
+    SKAction *animateScoreBoard = [SKAction group:@[[SKAction scaleTo:1.0 duration:0.5], fadeIn]];
+    animateScoreBoard.timingMode = SKActionTimingEaseOut;
+    [_scoreBoard runAction:animateScoreBoard];
+    
+    _playButton.alpha = 0.0;
+    SKAction *animatePlayButton = [SKAction fadeInWithDuration:2.0];
+    animatePlayButton.timingMode = SKActionTimingEaseIn;
+    [_playButton runAction:animatePlayButton completion:^{
+        self.touchable = YES;
+    }];
+    
     self.touchable = YES;
 }
 
