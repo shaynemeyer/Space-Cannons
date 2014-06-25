0088d5e78e4c3e7741bed114a3a3904f48a2c6b8
Shayne Meyer <shayne.meyer@gmail.com>
2014-06-25 05:39:51 -0700

    fixed crash bug related to sound on an actual device.

--- Space Cannons/AppDelegate.m
+++ Space Cannons/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import <SpriteKit/SpriteKit.h>
 #import "MyScene.h"
+#import <AVFoundation/AVFoundation.h>
 
 @implementation AppDelegate
 
@@ -22,6 +23,9 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    [[AVAudioSession sharedInstance] setActive:NO error:nil];
+    
     SKView *view = (SKView*)self.window.rootViewController.view;
     ((MyScene*)view.scene).gamePaused = YES;
 }
@@ -30,11 +34,13 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [[AVAudioSession sharedInstance] setActive:NO error:nil];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [[AVAudioSession sharedInstance] setActive:YES error:nil];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
